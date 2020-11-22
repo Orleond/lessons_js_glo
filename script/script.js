8fef38e17fb9f87bbcc41b4517bee3c6d233f3fc
Mr. Orleond <study.mr.orleond>
2020-11-23 02:51:50 +0300

    Второй коммит

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
-let money = 123;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert(money);
-console.log('Интересненько, однако');
+let money = 777777777;
+let income = 'Инвестиции';
+let addExpenses = 'Коммуналка, интернет, телефон, аренда, цветы жене';
+let deposit = true;
+let mission = 10000000;
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = 50;
+console.log(budgetDay);
